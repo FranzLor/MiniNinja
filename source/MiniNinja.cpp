d87f0b05cc4382f403f6fca41601a32c5f687b77
ZeroCalBacon <FranzLor64@yahoo.com>
2024-05-08 11:07:09 -0600

    Feature: Player -> rendering ninja

--- source/MiniNinja.cpp
+++ source/MiniNinja.cpp
@@ -2,6 +2,8 @@
 #include <raylib.h>
 #include <raymath.h>
 
+
+
 int main(void) {
 	//INIT
 	constexpr int windowWidth = 390;
@@ -18,7 +20,20 @@ int main(void) {
 	Texture2D map = LoadTexture("assets/map.png");
 	Vector2 mapPosition = { 0.0f, 0.0f };
 
+	//ninja - player
+	Texture2D ninja = LoadTexture("assets/ninja_sprite.png");
+	//attributes
 	float speed = 4.0f;
+	float scale = 4.0f;
+	//animation
+	int frameWidth = ninja.width / 4;
+	int frameHeight = ninja.height / 7;
+
+
+	Vector2 ninjaPos = {
+		static_cast<float>((windowWidth) - frameWidth * scale) / 2,
+		static_cast<float>((windowHeight) - frameHeight * scale) / 2
+	};
 
 	SetTargetFPS(60);
 
@@ -30,25 +45,48 @@ int main(void) {
 
 		Vector2 direction = { 0.0f, 0.0f };
 
+		//UPDATE
+		//WASD movement
 		if (IsKeyDown(KEY_W)) {
-			direction.x -= 1.0f;
+			direction.y -= 1.0f;
 		}
 		if (IsKeyDown(KEY_D)) {
 			direction.x += 1.0f;
+
 		}
 		if (IsKeyDown(KEY_S)) {
 			direction.y += 1.0f;
+
 		}
 		if (IsKeyDown(KEY_A)) {
 			direction.x -= 1.0f;
+
 		}
 
 		if (Vector2Length(direction) != 0.0f) {
 			mapPosition = Vector2Subtract(mapPosition, Vector2Scale(Vector2Normalize(direction), speed));
 		}
 
+		//RENDER
+		//map
 		const float mapScale = 4.0f;
 		DrawTextureEx(map, mapPosition, 0.0f, mapScale, WHITE);
+
+		//ninja
+		Rectangle playerSrc = {
+				0.0f,
+				0.0f,
+				static_cast<float>(frameWidth),
+				static_cast<float>(frameHeight)
+		};
+		Rectangle playerDst = {
+			ninjaPos.x,
+			ninjaPos.y,
+			frameWidth * scale,
+			frameHeight * scale
+		};
+		Vector2 playerOrg = { 0.0f, 0.0f };
+		DrawTexturePro(ninja, playerSrc, playerDst, playerOrg, 0.0f, WHITE);
 
 		EndDrawing();
 	}
